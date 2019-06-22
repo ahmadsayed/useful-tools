7ede6b37f7fe3b7bda3cdd320b39066963b174f0
root <root@openshift.IBM.cloud>
2019-06-22 07:26:13 +0000

    first commit

---
+++ install-docker-ubuntu-1804.sh
@@ -0,0 +1,11 @@
+echo "Update apt repo"
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "Add Docker bionic Repository"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+echo "Install docker-ce"
+sudo apt -y install docker-ce
+
