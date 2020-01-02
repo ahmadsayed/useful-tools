2faf5cb6d9ec1dfb74a9dde2dedfbd35ecf0ee18
Ahmed Sayed <Ahmed.Sayed.Hassan@ibm.com>
2020-01-02 17:02:25 +0800

    Update install-docker-ubuntu-1804.sh

--- install-docker-ubuntu-1804.sh
+++ install-docker-ubuntu-1804.sh
@@ -1,6 +1,6 @@
 echo "Update apt repo"
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 echo "Add Docker bionic Repository"
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
